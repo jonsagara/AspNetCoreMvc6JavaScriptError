cc59830030f6392d02fd49d8e43c26a16d0693a6
Jon Sagara <jon@sagarasoftware.com>
2021-12-09 12:24:18 -0800

    Initial commit.

---
+++ src/AspNetCoreMvc6JavaScriptError/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreMvc6JavaScriptError.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/AspNetCoreMvc6JavaScriptError/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreMvc6JavaScriptError.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
